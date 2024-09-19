91907093d41b40201f564bb4def4d2fdb2799869
lyndsie165 <122139049+lyndsie165@users.noreply.github.com>
2024-09-18 17:49:51 -0700

    Update Project3.py

--- Project3.py
+++ Project3.py
@@ -7,11 +7,11 @@ rawdf = pd.read_csv('movies.csv')
 #clean data
 cleanedDF = rawdf.dropna()
  
-#MAIN QUESTION: How do Score and Budget affect Revenue in the Movie industry? 
+#MAIN QUESTION: How do Score and Budget affect Revenue in the Movie industry 1980-2019? 
 
 #FACTOR 1: SCORE
 #Question: Does movies with higher score have higher revenue? 
-#Use Scatterplot between Score and Revenue:
+
 fig1 = px.scatter(cleanedDF, x='score', y='gross', 
                     labels={
                         'score': 'Score',
@@ -19,13 +19,10 @@ fig1 = px.scatter(cleanedDF, x='score', y='gross',
                     },
                     title='Gross Revenue versus Score')
 fig1.show()
-#Answer: Movies with higher score does not always generate higher revenue. At score=7 for example, 
-#a movie can bring in revenue as high as 1.67B or as low as 24.963K USD. 
-    #RESULT: Higher score does not mean higher Revenue. 
 
 #FACTOR 2: BUDGET
 #Question: Does movies with higher budget have higher revenue? 
-#Create another Scatterplot between Budget and Revenue:
+
 fig2 = px.scatter(cleanedDF, x='budget', y='gross', opacity=0.5,
                     labels={
                         'budget': 'Budget',
@@ -33,10 +30,6 @@ fig2 = px.scatter(cleanedDF, x='budget', y='gross', opacity=0.5,
                     },
                     title='Gross Revenue versus Budget')
 fig2.show()
-#Answer: Movies with higher budget generate higher revenue while movies with lower budget generate
-# less revenue, with a few exceptions (outliers).
-    #RESULT: Budget affects Revenue. 
-
 
 #Findings with Country
 #Ques_1: Which country generates highest revenue in movie industry?
@@ -52,7 +45,6 @@ fig3 = px.bar(sum, x='country', y='gross', color='country',
                 title='Revenue of each Countries')
 fig3.update_layout(barmode='stack', xaxis={'categoryorder':'total ascending'})
 fig3.show()
-#Answer: United States generates highest revenue (470.79 billion USD).
 
 #Ques_2: Is it because the US having the highest budget?
 #First, find the average budget of each country:
@@ -68,8 +60,6 @@ fig4 = px.bar(average, x='country', y='budget', color='budget',
                 title='Average Budget in Countries')
 fig4.update_layout(barmode='stack', xaxis={'categoryorder':'total ascending'})
 fig4.show()
-#Answer: US (highest revenue) is not the one having highest budget. It is China with 71 million USD.
-#Meaning budget is not the reason why the US had highest revenue. 
 
 #Ques_3: Is it because the US produce the highest number of movies?
 #Calculate the total number of movies produced in each countries:
@@ -77,8 +67,6 @@ countUS = cleanedDF.groupby('country')['year'].count().reset_index()
 countUS = countUS.sort_values('year')
 renamed_US = countUS.rename(columns={'year':'No of movies'})
 print(renamed_US)
-#Answer: US produces the highest number of movies.
-#Meaning United States with highest movie productions generates highest revenue. 
 
 #Findings with Genre
 #Ques_1: Gross revenue for each genre of movies?
@@ -90,7 +78,6 @@ fig5 = px.histogram(cleanedDF, x='genre', y='gross',
                     },
                     title='Gross Revenue based on Genre of movies')
 fig5.show()
-#Answer: Overall, Action generates highest revenue, then Comedy and Animation rank 2nd and 3rd. 
 
 #Ques_2: How does the revenue vary from time to time, based on Genre?
 #As data is inadequate in 2020 and after:
@@ -104,13 +91,10 @@ print(cleanedDF)
 #Use line chart to show the trend:
 fig6 = px.line(cleanedDF, x="year", y="gross", color='genre')
 fig6.show()
-#Answer: Revenue of each Genre fluctuates from 1980 to 2019, with Action generates highest revenue 
-#most of the years, followed by Animation and Comedy. 
 
 #Ques_3: How many movies produced in each kind of genres?
 countGE = cleanedDF.groupby('genre')['year'].count().reset_index()
 countGE = countGE.sort_values('year')
 renamed_GE = countGE.rename(columns={'year':'count'})
 print(renamed_GE)
-#Answer: see table.
 
